1cf28d98dc77b51e1ba81632bfdcf567fd64cd24
MatiasPenalozaJara <matiaspenalozajarapj6@gmail.com>
2022-12-12 22:45:27 -0300

    viaje arreglado

--- src/app/pages/crear-viaje/crear-viaje.page.ts
+++ src/app/pages/crear-viaje/crear-viaje.page.ts
@@ -11,20 +11,20 @@ import { DbservicioService } from 'src/app/services/dbservicio.service';
 })
 export class CrearViajePage implements OnInit {
   fotocon: any;
-  
-  asientos='';
 
-  radioSelected='';
-  tarifa='';
-  comun='';
-  idextras='';
-  nombreextras='';
-  claveextras='';
-  fotoextras='';
-  idrolextras='';
-  patentextras='';
-  fkextras='';
-  marcaextras='';
+  asientos = '';
+
+  radioSelected = '';
+  tarifa = '';
+  comun = '';
+  idextras = '';
+  nombreextras = '';
+  claveextras = '';
+  fotoextras = '';
+  idrolextras = '';
+  patentextras = '';
+  fkextras = '';
+  marcaextras = '';
 
   a = null;
   b = null;
@@ -35,9 +35,9 @@ export class CrearViajePage implements OnInit {
   Usuario: any[] = []
   Auto: any[] = []
   Viaje: any[] = []
-  constructor(private activedRouter: ActivatedRoute,private bd: DbservicioService, private api: CameraService, public nativeStorage: NativeStorage, private router: Router) {
-    this.activedRouter.queryParams.subscribe(param=>{
-      if(this.router.getCurrentNavigation().extras.state){
+  constructor(private activedRouter: ActivatedRoute, private bd: DbservicioService, private api: CameraService, public nativeStorage: NativeStorage, private router: Router) {
+    this.activedRouter.queryParams.subscribe(param => {
+      if (this.router.getCurrentNavigation().extras.state) {
         this.idextras = this.router.getCurrentNavigation().extras.state.idenviado;
         this.nombreextras = this.router.getCurrentNavigation().extras.state.nombreenviado;
         this.claveextras = this.router.getCurrentNavigation().extras.state.claveenviado;
@@ -50,7 +50,7 @@ export class CrearViajePage implements OnInit {
   }
 
 
-ngOnInit() {
+  ngOnInit() {
     this.bd.dbState().subscribe((res) => {
       if (res) {
         this.bd.fetchUser().subscribe(item => {
@@ -59,59 +59,64 @@ ngOnInit() {
         })
       }
     })
-    this.bd.dbState().subscribe((res)=>{
-      if(res){
-        this.bd.fetchauto().subscribe(item =>{
+    this.bd.dbState().subscribe((res) => {
+      if (res) {
+        this.bd.fetchauto().subscribe(item => {
           this.Auto = item;
         })
       }
     })
-    this.bd.dbState().subscribe((res)=>{
-      if(res){
-        this.bd.fetchViaje().subscribe(item =>{
+    this.bd.dbState().subscribe((res) => {
+      if (res) {
+        this.bd.fetchViaje().subscribe(item => {
           this.Viaje = item;
         })
       }
     })
 
-}//FINAL ONINIT
+  }//FINAL ONINIT
 
 
 
-crearviaje(){
-  // if(this.asientos < '4' && this.asientos == '0')
+  crearviaje() {
+    // if(this.asientos < '4' && this.asientos == '0')
+    
+    // let navigationExtras: NavigationExtras = {
+    //   state: {
+    //     nombreenviadov: this.nombreextras,
+    //     patenteenviadov: this.radioSelected,
+    //     comunaenviadov: this.comun,
+    //     costoenviadov: this.tarifa,
+    //     asientoenviadov: this.asientos
+    //   }
+    // }
+    if (this.asientos > '4'){
+      this.bd.presentAlert('Los asientos deben ser menos que 4')
+    this.router.navigate(['/inicio-conductor'])
+  }
+  else if (this.asientos < '1'){
+      this.bd.presentAlert('Los asientos deben ser más de 1')
+    }
+    else{
       this.nativeStorage.setItem('nombreViaje', this.nombreextras);
-      this.nativeStorage.setItem('patenteViaje', this.radioSelected);
-      this.nativeStorage.setItem('comunaViaje',this.comun);
-      this.nativeStorage.setItem('costoViaje', this.tarifa);
-      this.nativeStorage.setItem('asientos', this.asientos);
-      // let navigationExtras: NavigationExtras = {
-      //   state: {
-      //     nombreenviadov: this.nombreextras,
-      //     patenteenviadov: this.radioSelected,
-      //     comunaenviadov: this.comun,
-      //     costoenviadov: this.tarifa,
-      //     asientoenviadov: this.asientos
-      //   }
-      // }
-  if(this.asientos > '4')
-    this.bd.presentAlert('Los asientos deben ser menos que 4')
-  else if(this.asientos < '1')
-    this.bd.presentAlert('Los asientos deben ser más de 1')
-  else
-  this.bd.agregarviaje(this.nombreextras,this.comun,this.tarifa,this.radioSelected,this.asientos)
-  this.bd.presentAlert("Viaje creado")
-  this.router.navigate(['/inicio-conductor'])
-}
+    this.nativeStorage.setItem('patenteViaje', this.radioSelected);
+    this.nativeStorage.setItem('comunaViaje', this.comun);
+    this.nativeStorage.setItem('costoViaje', this.tarifa);
+    this.nativeStorage.setItem('asientos', this.asientos);
+      this.bd.agregarviaje(this.nombreextras, this.comun, this.tarifa, this.radioSelected, this.asientos)
+    this.bd.presentAlert("Viaje creado")
+    this.router.navigate(['/inicio-conductor'])
+  }
+  }
 
-Volver(){
-  this.nativeStorage.setItem('nombreViaje', this.a);
-  this.nativeStorage.setItem('patenteViaje', this.b);
-  this.nativeStorage.setItem('comunaViaje',this.c);
-  this.nativeStorage.setItem('costoViaje', this.d);
-  this.nativeStorage.setItem('asientos', this.e);
-  this.router.navigate(['/inicio-conductor'])
-}
+  Volver() {
+    this.nativeStorage.setItem('nombreViaje', this.a);
+    this.nativeStorage.setItem('patenteViaje', this.b);
+    this.nativeStorage.setItem('comunaViaje', this.c);
+    this.nativeStorage.setItem('costoViaje', this.d);
+    this.nativeStorage.setItem('asientos', this.e);
+    this.router.navigate(['/inicio-conductor'])
+  }
 
 
 }//FINAL;
